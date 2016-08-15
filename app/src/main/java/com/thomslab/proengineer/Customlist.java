99ed4b78fac869d67c07a3fa9a8a821ad4952431
thomslab <hidayatthomi@gmail.com>
2016-08-15 19:59:23 +0700

    berhasil membuat listview pada electrical fragment

--- app/src/main/java/com/thomslab/proengineer/Customlist.java
+++
@@ -1,8 +0,0 @@
-package com.thomslab.proengineer;
-
-/**
- * Created by mitohida on 8/14/2016.
- */
-
-public class Customlist {
-}
--- app/src/main/java/com/thomslab/proengineer/electrical.java
+++ app/src/main/java/com/thomslab/proengineer/electrical.java
@@ -5,19 +5,27 @@ package com.thomslab.proengineer;
  */
 
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 //Our class extending fragment
-public class electrical extends Fragment {
-   private ListView listView;
+public class electrical extends Fragment{
+
+    String[] listTitle;
+    TypedArray ImageTitle;
+
+    List<MenuRowItem> menuRowItems;
+    ListView Electrical_Listview;
+
 
     //Overriden method onCreateView
     @Override
@@ -26,16 +34,24 @@ public class electrical extends Fragment {
         //Returning the layout file after inflating
         //Change R.layout.tab1 in you classes
         View view = inflater.inflate(R.layout.electrical_tab, container, false);
+        menuRowItems = new ArrayList<MenuRowItem>();
+
+        listTitle = getResources().getStringArray(R.array.electrical_menu);
+
+        ImageTitle = getResources().obtainTypedArray(R.array.icons);
+
+        for (int i = 0; i < listTitle.length; i++) {
+            MenuRowItem item = new MenuRowItem(ImageTitle.getResourceId(i,-1),listTitle[i]);
+            menuRowItems.add(item);
+        }
+
+        Electrical_Listview = (ListView) view.findViewById(R.id.list_electrical);
+        CustomAdapter adapter = new CustomAdapter(getContext(), menuRowItems);
+        Electrical_Listview.setAdapter(adapter);
 
 
         return view;
 
-
-
-
-
-
     }
-
 
 }
